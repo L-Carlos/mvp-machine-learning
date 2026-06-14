b48bb13b1416a9c0448dc66030a4a379db05cc45
L-Carlos <luiscarlosfirminopinheiro@gmail.com>
2026-06-14 14:32:06 -0300

    script de consolidacao de arquivos e inicio do readme

---
+++ README.md
@@ -0,0 +1,22 @@
+### MVP - Machine Learning & Analytics PUC RJ
+
+**Nome:** Luis Carlos Firmino Pinheiro  
+**Matrícula:** 4052026000838  
+**Data:** 13/06/2026  
+**Dataset:** [SINISTROS DE TRÂNSITO - PRF](https://dados.gov.br/dados/conjuntos-dados/sinistros-de-transito-agrupados-por-ocorrencia)  
+**Tipo de problema:** Classificação  
+
+# Modelo de classificação de acidentes de trânsito em rodovias brasileiras
+
+![header](image/README/header.jpg)
+
+### O objetivo desse estudo, é a criação de um modelo que atue com base em informações escassas disponiveis no momento de uma ocorência, para determinar a gravidade e dimensionar a resposta adequada.
+
+A ideia é que o modelo classifique sem saber ainda exatamente o que ocorreu, como uma espécie de *"Early Warning System"*, que poderia atuar com fontes de deteccão de ocorrência como sensores de pista, cameras, ou descrição de testemunhas que passaram pelo local.
+
+Além das informações básicas como horário, data, localização, o dataset utilizado possui informações como causa e quantidade de vitimas, que são preenchidas posteriormente durante o registro da ocorrência, essas e outras variaveis posteriores não serão utilizada no modelo,   justamente porque o modelo seria utilizando antes desse tipo de informação estar disponível.  
+
+## Sobre o Dataset
+
+Os dados que serão utilizados são provenientes do [Portal Brasileiro de Dados Abertos](https://dados.gov.br/home), no dataset [SINISTROS DE TRÂNSITO - PRF](https://dados.gov.br/dados/conjuntos-dados/sinistros-de-transito-agrupados-por-ocorrencia).  
+Os dados originais são armazenados em um [google drive](https://drive.google.com/drive/folders/1eI64p7H7LRc-wWDnwjIdYmYbWurMbwbT) em subpastas com um csv para cada ano. O arquivo zip com todos os anos foi salvo em `./raw_data/dados_prf.zip`, e o script `./consolidar_arquivos.py` junta todos os zips e subpastas em um único arquivo `./data/dados_prf.parquet`, para facilitar a leitura no projeto.
---
+++ consolidar_arquivos.py
@@ -0,0 +1,78 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "pandas>=3.0.3",
+#     "pyarrow>=24.0.0",
+# ]
+# ///
+
+# Script para consolidar os dados em ./raw_data/dados_prf.zip
+# em um unico arquivo .parquet para ser carregado no github
+
+import io
+import zipfile
+
+import pandas as pd
+
+
+def consolidar_arquivos(zip_path: str, out_path: str):
+    dfs = []
+
+    print(f"Lendo arquivos em {zip_path} ...")
+
+    with zipfile.ZipFile(zip_path, mode="r") as main_zip:
+        for zip_info in main_zip.infolist():
+            if not zip_info.filename.lower().endswith(".zip"):
+                continue
+
+            sub_zip = main_zip.read(zip_info)
+
+            with zipfile.ZipFile(io.BytesIO(sub_zip), mode="r") as z:
+                for file_info in z.infolist():
+                    if file_info.is_dir():
+                        continue
+                    if not file_info.filename.lower().endswith(".csv"):
+                        continue
+
+                    with z.open(file_info) as f:
+                        temp_df = pd.read_csv(
+                            f,
+                            encoding="cp1252",
+                            sep=";",
+                            low_memory=False,
+                            # alguns campos causam erro no parquet se deixados como objects
+                            # depois serao tratados corretamente na etapa de modelagem
+                            dtype={
+                                "br": float,
+                                "km": str,
+                                "latitude": str,
+                                "longitude": str,
+                            },
+                            na_values=["(null)", "null", "NULL", "N/D", "N/A#"],
+                        )
+                        dfs.append(temp_df)
+
+    if not dfs:
+        print("Nenhum CSV encontrado. Verifique a estrutuda do .zip")
+        return
+
+    df = pd.concat(dfs, ignore_index=True)
+
+    print(f"DF final: {df.shape[0]} linhas, {df.shape[1]} colunas.")
+
+    print(df.head())
+
+    print(df.info())
+
+    print("Salvando arquivo final ...")
+
+    df.to_parquet(out_path, compression="snappy", index=False)
+
+    print(f"Processamento finalizado, arquivo salvo em {out_path}")
+
+
+if __name__ == "__main__":
+    ZIP_PATH = "./raw_data/dados_prf.zip"
+    DESTINATION_PATH = "./data/dados_prf.parquet"
+
+    consolidar_arquivos(ZIP_PATH, DESTINATION_PATH)
